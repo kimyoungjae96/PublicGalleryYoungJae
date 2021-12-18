383f21e029d80a6ccaf587b9ea9b018ed882bc71
kimyoungjae <sskim5421@gmail.com>
2021-12-18 20:37:47 +0900

    Add post card on feed screen

--- src/screens/FeedScreen.js
+++ src/screens/FeedScreen.js
@@ -1,8 +1,32 @@
-import React from 'react';
-import {View} from 'react-native';
+import React, {useEffect, useState} from 'react';
+import {View, FlatList} from 'react-native';
+import {getPosts} from '../lib/posts';
+import PostCard from '../components/PostCard';
 
 function FeedScreen() {
-  return <View />;
+  const [posts, setPosts] = useState(null);
+
+  useEffect(() => {
+    getPosts().then(setPosts);
+  }, []);
+
+  return (
+    <FlatList
+      data={posts}
+      renderItem={renderItem}
+      keyExtractor={item => item.id}
+    />
+  );
 }
+
+const renderItem = ({item}) => (
+  <PostCard
+    createdAt={item.createAt}
+    description={item.description}
+    id={item.id}
+    user={item.user}
+    photoURL={item.photoURL}
+  />
+);
 
 export default FeedScreen;
